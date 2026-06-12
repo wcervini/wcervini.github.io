57d4bb2a74f56448322567822e2096e8ac420d7b
Walter Cervini <wcervini@gmail.com>
2026-06-12 06:39:41 +0100

    feat: update dependencies and improve styling
    - Added new font dependencies: Bitcount Single Ink and Roboto.
    - Updated global styles to include Tailwind CSS and modified font-family for body and headings.
    - Changed header logo text from "wcervini" to "Walter Cervini".
    - Removed redundant global CSS import in BaseLayout.astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wcervini.github.io'
+  site: 'https://wcervini.github.io',
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
